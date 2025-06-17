6274b4e5bfe39628073ce8d0212370b9053f40d7
pedroGEOGIScoding <pedromartinezduran@gmail.com>
2025-06-17 10:04:06 +0200

    EventRecordRepository.java added. App opens and running fine.

---
+++ src/main/java/com/pedro/apps/events/EventRecordRepository.java
@@ -0,0 +1,12 @@
+package com.pedro.apps.events;
+
+import java.util.List;
+
+public interface EventRecordRepository {
+  
+  <T> T get(String partitionKey, String sortKey, Class<T> clazz);
+  <T> List<T> listByPartitionKey(String partitionKey, Class<T> clazz);
+  <T> List<T> listByPartitionKeyAndSortKey(String partitionKey, String sortKey, Class<T> clazz);
+  <T> List<T> listBySortKey(String sortKey, Class<T> clazz);
+  <T> List<T> listAll(Class<T> clazz);
+}
